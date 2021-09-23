63f4452421293ba56c34b15207ef0f2a35a2135f
Jack <jackjin701@gmail.com>
2021-09-23 12:30:58 +0800

    update library

--- contracts/Astronaut.sol
+++ contracts/Astronaut.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/IInventory.sol";
 import "./interfaces/IDarkMatter.sol";
@@ -13,7 +12,6 @@ import "./interfaces/IDarkMatter.sol";
  * @title Astronaut Contract
  */
 contract Astronaut is Ownable, ReentrancyGuard {
-    using Address for address;
     using SafeERC20 for IERC20;
 
     /// @notice Event emitted only on construction.
