7d5a19a94b26da489031d65931d85b8e14c7765a
Jack <jackjin701@gmail.com>
2021-09-23 11:04:19 +0800

    Update Astronaut

--- contracts/Astronaut.sol
+++ contracts/Astronaut.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/IInventory.sol";
+import "./interfaces/IDarkMatter.sol";
 
 /**
  * @title Astronaut Contract
@@ -15,46 +16,55 @@ contract Astronaut is Ownable, ReentrancyGuard {
     using Address for address;
     using SafeERC20 for IERC20;
 
-    IERC20 vidya;
-    IERC20 dm;
+    /// @notice Event emitted only on construction.
+    event AstronautDeployed();
 
-    IInventory public inventory =
-        IInventory(0x9680223F7069203E361f55fEFC89B7c1A952CDcc); // Mainnet address: 0x9680223f7069203e361f55fefc89b7c1a952cdcc
+    /// @notice Event emitted when astronaut created.
+    event AstronautCreated(
+        address user,
+        string name,
+        uint256 skill,
+        uint256 tokenId,
+        uint256 amount
+    );
 
-    uint256 constant fee = 5 * 10**18;
-    uint256 constant darkMatterDeci = 10**18;
-    uint256 constant expToBuy = 5 * 10**17;
-    address dev;
-    address vault;
+    /// @notice Event emitted when tank added.
+    event TanksAdded(
+        address user,
+        uint256 timeTillDeath,
+        uint256 tokenId,
+        uint256 amount,
+        uint256 skill
+    );
 
-    uint256 DMID = 18;
-    uint256 tankID = 36;
-
-    event tankRateChange(uint256 tankRate);
     event nameChange(string astroName);
     event durationChanged(uint256 duration);
     event astronautInSpace(address player, string spaceman);
-    event creation();
+
     event maxLevelAcheived(uint256 skill, uint256 levelReached);
     event harvestExtend(address player, uint256 lengthTillEnd);
     event experienceTrained(address player, uint256 skill, uint256 experience);
     event costChange(uint256 tankPrice);
 
-    modifier NotDeadYet() {
-        require(
-            players[msg.sender].timeTillDeath >= block.timestamp,
-            "Astronaut: Astronaut lost in Space."
-        );
-        _;
-    }
+    IERC20 Vidya;
+    IDarkMatter DarkMatter;
+    IInventory Inventory;
 
-    modifier AloneInSpace() {
-        require(
-            players[msg.sender].timeTillDeath < block.timestamp,
-            "Astronaut: Still in contact with SpaceMan"
-        );
-        _;
-    }
+    address public vaultAddr;
+    address public devAddr;
+
+    uint256 public constant fee = 5 * 10**18;
+    uint256 public constant darkMatterDeci = 10**18;
+    uint256 public constant expToBuy = 5 * 10**17;
+
+    uint256 public constant darkMatterId = 18;
+    uint256 public constant tankID = 36;
+    uint256 public constant maxLevel = 99;
+
+    uint256 public maxDarkMatterTank;
+    uint256 public tankDuration;
+    uint256 public costOfTank;
+    uint256[] public levels;
 
     struct Astronauts {
         string name;
@@ -65,108 +75,144 @@ contract Astronaut is Ownable, ReentrancyGuard {
         uint256 darkMatterCollectionRate;
         uint256 lastCollected;
         bool created;
-        uint256 DMBonus;
+        uint256 darkMatterBonus;
     }
 
-    uint256 maxDMTank;
-    uint256[] levels;
-    uint256 tankDuration;
-    uint256 constant maxLevel = 99;
-    uint256 costOfTank;
-    mapping(address => Astronauts) players;
+    mapping(address => Astronauts) public players;
 
+    modifier notDeadYet() {
+        require(
+            players[msg.sender].timeTillDeath >= block.timestamp,
+            "Astronaut: Astronaut lost in Space."
+        );
+        _;
+    }
+
+    modifier aloneInSpace() {
+        require(
+            players[msg.sender].timeTillDeath < block.timestamp,
+            "Astronaut: Still in contact with SpaceMan"
+        );
+        _;
+    }
+
+    /**
+     * @dev Constructor function
+     * @param _devAddr Developer Address
+     * @param _vaultAddr Vault Address
+     * @param _Vidya Interface of Vidya (0x3D3D35bb9bEC23b06Ca00fe472b50E7A4c692C30)
+     * @param _DarkMatter Interface of Dark Matter
+     * @param _Inventory Interface of Inventory
+     */
     constructor(
-        address _dev,
-        address _vault,
-        IERC20 _vidya,
-        IERC20 _DM
+        address _devAddr,
+        address _vaultAddr,
+        IERC20 _Vidya,
+        IDarkMatter _DarkMatter,
+        IInventory _Inventory
     ) {
-        dev = _dev;
-        vidya = _vidya;
-        dm = _DM;
-        vault = _vault;
+        devAddr = _devAddr;
+        Inventory = _Inventory;
+        Vidya = _Vidya;
+        DarkMatter = _DarkMatter;
+        vaultAddr = _vaultAddr;
+
         levels.push(1 * darkMatterDeci); //0-1
         levels.push(3 * darkMatterDeci); //1-2
         levels.push(6 * darkMatterDeci); //2-3
         levels.push(10 * darkMatterDeci); //3-4
         levels.push(15 * darkMatterDeci); //4-5
         levels.push(21 * darkMatterDeci); //5-6
-        emit creation();
+
+        emit AstronautDeployed();
     }
 
+    /**
+     * @dev External function to create Astronaut.
+     * @param _name Astronaut Name
+     * @param _skill Skill amount
+     * @param _tokenId Tank token Id
+     * @param _amount Tank amount
+     */
     function createAstronaut(
         string memory _name,
         uint256 _skill,
-        uint256[] memory _tokenID
-    ) public {
+        uint256 _tokenId,
+        uint256 _amount
+    ) external {
         require(
             !players[msg.sender].created,
-            "Astronaut: There can only be one."
+            "Astronaut: There can only be one"
         );
 
-        uint256 _numTanks = _tokenID.length;
-        require(_numTanks <= 15, "Astronaut: To many tanks");
+        require(_amount <= 15, "Astronaut: Too many tanks");
 
-        uint256 i = 0;
-
-        while (i < _tokenID.length) {
-            if (!burnNFT(tankID, _tokenID[i])) {
-                _numTanks -= 1;
-            }
-            i += 1;
-        }
+        Inventory.burn(msg.sender, _tokenId, _amount);
 
         Astronauts memory holder;
+
         holder.name = _name;
         holder.multiplier[_skill] = 95;
         holder.multiplier[(_skill + 1) % 3] = 100;
         holder.multiplier[(_skill + 2) % 3] = 100;
         holder.level[_skill] = 5;
-        holder.timeTillDeath = block.timestamp + (tankDuration * _numTanks);
+        holder.timeTillDeath = block.timestamp + (tankDuration * _amount);
         holder.lastCollected = block.timestamp;
-        holder.darkMatterCollectionRate = DMcollectionRate(100, 100, 95);
-        holder.DMBonus = 100;
+        holder.darkMatterCollectionRate = DMCollectionRate(100, 100, 95);
+        holder.darkMatterBonus = 100;
         holder.created = true;
 
         players[msg.sender] = holder;
 
-        emit astronautInSpace(msg.sender, _name);
+        emit AstronautCreated(msg.sender, _name, _skill, _tokenId, _amount);
     }
 
-    function addTanks(uint256[] memory _tokenID, uint256 _skill)
-        public
-        NotDeadYet
-        nonReentrant
-    {
-        uint256 _numTanks = _tokenID.length;
-        require(_numTanks <= 15, "Astronaut: To many tanks");
-        uint256 i = 0;
-        while (i < _tokenID.length) {
-            if (!burnNFT(tankID, _tokenID[i])) {
-                _numTanks -= 1;
-            }
-            i += 1;
-        }
+    /**
+     * @dev External function to add tanks.
+     * @param _tokenId Tank token Id
+     * @param _amount Tank amount
+     * @param _skill Skill amount
+     */
+    function addTanks(
+        uint256 _tokenId,
+        uint256 _amount,
+        uint256 _skill
+    ) external notDeadYet {
+        require(_amount <= 15, "Astronaut: Too many tanks");
+
+        Inventory.burn(msg.sender, _tokenId, _amount);
+
         collectDarkMatter(_skill);
-        uint256 time = tankDuration * _numTanks;
+
+        uint256 time = tankDuration * _amount;
+
         players[msg.sender].timeTillDeath += time;
 
-        emit harvestExtend(msg.sender, players[msg.sender].timeTillDeath);
+        emit TanksAdded(
+            msg.sender,
+            players[msg.sender].timeTillDeath,
+            _tokenId,
+            _amount,
+            _skill
+        );
     }
 
-    function harvestDarkMatter(uint256 _skill) public NotDeadYet nonReentrant {
+    function harvestDarkMatter(uint256 _skill) public notDeadYet nonReentrant {
         collectDarkMatter(_skill);
     }
 
     function collectDarkMatter(uint256 _skill) internal {
         Astronauts storage user = players[msg.sender];
 
-        uint256 darkMatter = (user.DMBonus *
+        uint256 darkMatter = (user.darkMatterBonus *
             (block.timestamp - user.lastCollected) *
             user.darkMatterCollectionRate) / 100;
+
         user.lastCollected = block.timestamp;
         user.experience[_skill] += darkMatter;
+
         uint256 level = user.level[_skill];
+
         if (level < maxLevel) {
             if (user.experience[_skill] >= levels[level]) {
                 user.level[_skill] = level + 1;
@@ -177,17 +223,19 @@ contract Astronaut is Ownable, ReentrancyGuard {
         } else {
             emit maxLevelAcheived(_skill, maxLevel);
         }
-        user.darkMatterCollectionRate = DMcollectionRate(
+
+        user.darkMatterCollectionRate = DMCollectionRate(
             user.multiplier[0],
             user.multiplier[1],
             user.multiplier[2]
         );
-        dm.mint(msg.sender, darkMatter); //mint dark matter
+
+        DarkMatter.mint(msg.sender, darkMatter); //mint dark matter
     }
 
-    function rescueAstronaut(uint256[] memory _tokenID)
+    function rescueAstronaut(uint256 _tokenId, uint256 _amount)
         public
-        AloneInSpace
+        aloneInSpace
         nonReentrant
     {
         require(
@@ -195,20 +243,14 @@ contract Astronaut is Ownable, ReentrancyGuard {
             "Astronaut: There can only be one."
         );
 
-        uint256 _numTanks = _tokenID.length;
-        require(_numTanks <= 15, "Astronaut: To many tanks");
-        vidya.safeTransferFrom(msg.sender, vault, fee);
-        uint256 i = 0;
-        while (i < _tokenID.length) {
-            if (!burnNFT(tankID, _tokenID[i])) {
-                _numTanks -= 1;
-            }
-            i += 1;
-        }
+        require(_amount <= 15, "Astronaut: Too many tanks");
+        Vidya.safeTransferFrom(msg.sender, vaultAddr, fee);
+
+        Inventory.burn(msg.sender, _tokenId, _amount);
 
         Astronauts storage user = players[msg.sender];
 
-        for (i = 0; i < 3; i++) {
+        for (uint256 i = 0; i < 3; i++) {
             uint256 level = user.level[i];
             if (level % 2 == 1) {
                 level += 1;
@@ -219,83 +261,83 @@ contract Astronaut is Ownable, ReentrancyGuard {
             user.experience[i] = 0;
         }
         user.lastCollected = block.timestamp;
-        user.darkMatterCollectionRate = DMcollectionRate(
+        user.darkMatterCollectionRate = DMCollectionRate(
             user.multiplier[0],
             user.multiplier[1],
             user.multiplier[2]
         );
-        user.timeTillDeath = block.timestamp + (_numTanks * tankDuration);
+        user.timeTillDeath = block.timestamp + (_amount * tankDuration);
     }
 
-    function darkMatterNFTBonus(uint256[] memory _tokenID, uint256 _skill)
-        public
-        nonReentrant
-    {
-        uint256 _amountDM = _tokenID.length;
-        require(_amountDM <= 15, "Astronaut: To much Dark Matter at once.");
+    function darkMatterNFTBonus(
+        uint256 _tokenId,
+        uint256 _amount,
+        uint256 _skill
+    ) public nonReentrant {
+        require(_amount <= 15, "Astronaut: Too much Dark Matter at once.");
 
-        uint256 i = 0;
+        Inventory.burn(msg.sender, _tokenId, _amount);
 
-        while (i < _tokenID.length) {
-            if (!burnNFT(DMID, _tokenID[i])) {
-                _amountDM -= 1;
-            }
-            i += 1;
-        }
-        players[msg.sender].DMBonus += (2 * _amountDM);
+        players[msg.sender].darkMatterBonus += (2 * _amount);
+
         collectDarkMatter(_skill);
     }
 
     function removeDarkMatterBonus(uint256 _amount) public nonReentrant {
         Astronauts storage user = players[msg.sender];
-        uint256 nftOwned = user.DMBonus - 100;
+        uint256 nftOwned = user.darkMatterBonus - 100;
         uint256 removeBonus = _amount * 2;
         if (nftOwned >= removeBonus) {
             while (_amount > 0) {
-                inventory.createFromTemplate(DMID, 0, 0, 0, 0, 0);
+                Inventory.createItemFromTemplate(
+                    darkMatterId,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    1,
+                    msg.sender
+                );
                 _amount -= 1;
             }
         }
-        user.DMBonus = 100 + nftOwned - removeBonus;
+        user.darkMatterBonus = 100 + nftOwned - removeBonus;
     }
 
     /**
      * @dev trains the astronaut
      * @param _amount is only used when training skill 0, 1
-     * @param _tokenID is only used to train skill 2
+     * @param _skill is only used when training skill 0, 1
+     * @param _tokenId is only used to train skill 2
+     * @param _tokenAmount is only used to train skill 2
      */
-
     function trainAstronaut(
         uint256 _amount,
         uint256 _skill,
-        uint256[] memory _tokenID
-    ) public NotDeadYet nonReentrant {
+        uint256 _tokenId,
+        uint256 _tokenAmount
+    ) public notDeadYet nonReentrant {
         require(_skill <= 2, "Astronaut: Skill outside of range");
         uint256 experienceGained;
         if (_skill == 0) {
-            vidya.safeTransferFrom(msg.sender, vault, _amount);
+            Vidya.safeTransferFrom(msg.sender, vaultAddr, _amount);
             experienceGained = (_amount * expToBuy) / costOfTank;
         }
         if (_skill == 1) {
             require(
-                dm.balanceOf(msg.sender) >= _amount,
+                DarkMatter.balanceOf(msg.sender) >= _amount,
                 "Astronaut: Not enough Dark Matter."
             );
-            dm.burn(msg.sender, _amount);
-            experienceGained = (_amount * expToBuy) / maxDMTank;
+            DarkMatter.burn(msg.sender, _amount);
+            experienceGained = (_amount * expToBuy) / maxDarkMatterTank;
         }
         if (_skill == 2) {
-            uint256 _numTanks = _tokenID.length;
-            require(_numTanks <= 15, "Astronaut: To many tanks");
+            require(_tokenAmount <= 15, "Astronaut: Too many tanks");
 
-            uint256 i = 0;
-            while (i < _tokenID.length) {
-                if (!burnNFT(tankID, _tokenID[i])) {
-                    _numTanks -= 1;
-                }
-                i += 1;
-            }
-            experienceGained = _numTanks * expToBuy;
+            Inventory.burn(msg.sender, _tokenId, _amount);
+
+            experienceGained = _tokenAmount * expToBuy;
         }
         players[msg.sender].experience[_skill] += experienceGained;
         collectDarkMatter(_skill);
@@ -304,16 +346,19 @@ contract Astronaut is Ownable, ReentrancyGuard {
     }
 
     /**
-     * @dev returns how much dm is mined per block by the user.
+     * @dev Internal function to return how much dark matter is mined per block by the user.
+     * @param _speed Speed attribution
+     * @param _harvest Harvest attribution
+     * @param _oxygenEquipmet Oxygen Equipment attribution
+     * @return How much dark matter is mined
      */
-
-    function DMcollectionRate(
-        uint256 speed,
-        uint256 harvest,
-        uint256 oxygen_equipmet
+    function DMCollectionRate(
+        uint256 _speed,
+        uint256 _harvest,
+        uint256 _oxygenEquipmet
     ) internal view returns (uint256) {
-        //100**3 is used to simulate multipling % together
-        return ((maxDMTank * (1000000 - (speed * harvest * oxygen_equipmet))) /
+        return ((maxDarkMatterTank *
+            (1000000 - (_speed * _harvest * _oxygenEquipmet))) /
             (1000000 * tankDuration));
     }
 
@@ -327,25 +372,9 @@ contract Astronaut is Ownable, ReentrancyGuard {
         }
     }
 
-    function burnNFT(uint256 _template, uint256 _tokenID)
-        internal
-        view
-        returns (bool)
-    {
-        if (inventory.ownerOf(_tokenID) == msg.sender) {
-            uint256[] memory tempID = inventory.getTemplateIDsByTokenIDs(
-                [_tokenID]
-            );
-            if (_template == tempID[0]) {
-                return inventory.burn(_tokenID);
-            }
-        }
-        return false;
-    }
-
-    function changeName(string memory _name) public NotDeadYet nonReentrant {
-        vidya.safeTransferFrom(msg.sender, vault, fee);
-        vidya.safeTransferFrom(msg.sender, dev, fee);
+    function changeName(string memory _name) public notDeadYet nonReentrant {
+        Vidya.safeTransferFrom(msg.sender, vaultAddr, fee);
+        Vidya.safeTransferFrom(msg.sender, devAddr, fee);
 
         players[msg.sender].name = _name;
         emit nameChange(_name);
@@ -357,7 +386,7 @@ contract Astronaut is Ownable, ReentrancyGuard {
     }
 
     function changeTankRate(uint256 _DMamount) public onlyOwner {
-        // maxDMTank = _DMamount * scale;
+        // maxDarkMatterTank = _DMamount * scale;
         // emit tankRateChange(_DMamount);
     }
 
---
+++ contracts/interfaces/IDarkMatter.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.6;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title DarkMatter Interface
+ */
+interface IDarkMatter is IERC20 {
+    /**
+     * @dev External function to mint the token.
+     * @param _user Address of user
+     * @param _amount Token amount
+     */
+    function mint(address _user, uint256 _amount) external;
+
+    /**
+     * @dev External function to burn the token.
+     * @param _user Address of user
+     * @param _amount Token amount
+     */
+    function burn(address _user, uint256 _amount) external;
+}
