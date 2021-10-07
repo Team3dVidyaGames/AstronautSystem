a8305ed18e0d696c4c2fca206ebfbdf83e49655f
Jack <jackjin701@gmail.com>
2021-10-07 09:39:24 +0800

    Update interface

--- contracts/SpaceStation.sol
+++ contracts/SpaceStation.sol
@@ -3,12 +3,20 @@ pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/IDarkMatter.sol";
 import "./interfaces/IInventory.sol";
 
+/**
+ * @title SpaceStation Contract
+ */
 contract SpaceStation is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
+    /// @notice Event emitted only on construction.
+    event SpaceStationDeployed();
+
     event Launched(address sender, uint256 ID, string name);
     event RateChange(uint256 rate);
     event DarkMatterCollected(
@@ -20,66 +28,58 @@ contract SpaceStation is Ownable, ReentrancyGuard {
     event LevelsCreated(uint256[100] levels);
     event Training(address trainer, uint256[4] features, uint256 tokenID);
 
-    uint256 dmPerBlockish;
-    uint256 numberAstorsnauts;
-    uint256 adjustedDMRate;
-    uint256 astronautID;
-    uint256 O2tanksID;
-    uint256 DMID;
-    uint256[100] LVLS;
-    address vault;
-    IInventory Inventory;
-
-    mapping(uint256 => collectionData) astronauts;
-
-    mapping(string => bool) astronautsNames;
-
-    IDarkMatter DarkMatter;
-    IERC20 Vidya;
-
-    struct Item {
-        uint256 templateId;
-        uint8 feature1;
-        uint8 feature2;
-        uint8 feature3;
-        uint8 feature4;
-        uint8 equipmentPosition;
-        bool burned;
-    }
-
-    struct collectionData {
+    struct CollectionData {
         string name;
+        bool registered;
         uint256 rate;
         uint256 collectionStart;
-        bool registered;
         uint256 experience;
-        uint256[4] features;
         uint256[3] experiences;
         uint256[3] levels;
+        uint256[4] features;
     }
 
+    mapping(uint256 => CollectionData) public astronauts;
+    mapping(string => bool) public astronautsNames;
+
+    IInventory public Inventory;
+    IDarkMatter public DarkMatter;
+    IERC20 public Vidya;
+
+    uint256 public dmPerBlock;
+    uint256 public numberAstorsnauts;
+    uint256 public adjustedDmRate;
+    uint256 public astronautId;
+    uint256 public O2tanksId;
+    uint256 public dmId;
+    address public vault;
+    uint256[100] public levels;
+
     constructor(
-        uint256 _DmPerBlock,
-        address _vault,
         IInventory _Inventory,
-        IDarkMatter _dm,
-        IERC20 _vidya,
-        uint256 _astronautID,
-        uint256 _O2tanksID,
-        uint256 _DMID
+        IDarkMatter _DarkMatter,
+        IERC20 _Vidya,
+        address _vault,
+        uint256 _dmPerBlock,
+        uint256 _astronautId,
+        uint256 _O2tanksId,
+        uint256 _dmId
     ) {
-        O2tanksID = _O2tanksID;
-        dmPerBlockish = _DmPerBlock;
         Inventory = _Inventory;
-        DarkMatter = _dm;
-        Vidya = _vidya;
-        astronautID = _astronautID;
-        DMID = _DMID;
+        DarkMatter = _DarkMatter;
+        Vidya = _Vidya;
         vault = _vault;
-        LVLS[0] = 100 * 10**18;
-        LVLS[1] = 300 * 10**18;
-        LVLS[2] = 600 * 10**18;
-        LVLS[3] = 1000 * 10**18;
+        dmPerBlock = _dmPerBlock;
+        astronautId = _astronautId;
+        O2tanksId = _O2tanksId;
+        dmId = _dmId;
+
+        levels[0] = 100 * 10**18;
+        levels[1] = 300 * 10**18;
+        levels[2] = 600 * 10**18;
+        levels[3] = 1000 * 10**18;
+
+        emit SpaceStationDeployed();
     }
 
     function register(uint256 _tokenID, string memory _name)
@@ -87,26 +87,30 @@ contract SpaceStation is Ownable, ReentrancyGuard {
         nonReentrant
     {
         require(!astronautsNames[_name], "Space Station: Name already taken.");
-        Item memory astronaut = Inventory.allItems[_tokenID];
+        astronautsNames[_name] = true;
+
+        IInventory.Item memory astronaut = Inventory.allItems(_tokenID);
+
         require(
-            !astronaut.burned && (astronaut.templateID == astronautID),
+            !astronaut.burned && (astronaut.templateId == astronautId),
             "Space Station: Astronaut Does Not Exisit."
         );
         require(
             Inventory.balanceOf(msg.sender, _tokenID) > 0,
             "Space Station: Mission Controls not granted"
         );
-        collectionData storage data = astronauts[_tokenID];
+
+        CollectionData storage data = astronauts[_tokenID];
         require(!data.registered, "Space Station: Astronaut already in orbit");
         data.name = _name;
-        data.feature[0] = uint256(astronaut.feature1);
-        data.feature[1] = uint256(astronaut.feature2);
-        data.feature[2] = uint256(astronaut.feature3);
-        data.feature[3] = uint256(astronaut.feature4);
+        data.features[0] = uint256(astronaut.feature1);
+        data.features[1] = uint256(astronaut.feature2);
+        data.features[2] = uint256(astronaut.feature3);
+        data.features[3] = uint256(astronaut.feature4);
 
         data.rate =
-            (data.feature[0] + data.feature[1] + 1) *
-            (50 + data.feature[3]);
+            (data.features[0] + data.features[1] + 1) *
+            (50 + data.features[3]);
 
         data.collectionStart = block.timestamp;
         data.registered = true;
@@ -114,31 +118,32 @@ contract SpaceStation is Ownable, ReentrancyGuard {
         emit Launched(msg.sender, _tokenID, _name);
     }
 
-    function collectDarkMatter(uint256 _astronautID, uint256 numberOfTanks)
+    function collectDarkMatter(uint256 _astronautId, uint256 numberOfTanks)
         external
     {
         require(
-            Inventory.balanceOf(msg.sender, _astronautID) > 0,
+            Inventory.balanceOf(msg.sender, _astronautId) > 0,
             "Space Station: Permission not granted"
         );
         require(
-            Inventory.balanceOf(msg.sender, O2tanksID) >= numberOfTanks &&
+            Inventory.balanceOf(msg.sender, O2tanksId) >= numberOfTanks &&
                 numberOfTanks > 0,
             "Space Station: Miscalculation on O2 tanks"
         );
-        collectionData storage data = astronauts[_astronautID];
+        CollectionData storage data = astronauts[_astronautId];
         require(
             data.collectionStart <= block.timestamp,
             "Space Station: Astronaut not ready for Mission"
         );
 
         uint256 timeDifference = block.timestamp - data.collectionStart;
-        uint256 dmToMint = (data.rate * adjustedDMRate * timeDifference) / 500;
+        uint256 dmToMint = (data.rate * adjustedDmRate * timeDifference) / 500;
 
-        Inventory.burn(msg.sender, numberOfTanks);
+        Inventory.burn(msg.sender, _astronautId, numberOfTanks);
+
         data.collectionStart =
             block.timestamp +
-            ((256 - data.feature[2]) * timeDifference) /
+            ((256 - data.features[2]) * timeDifference) /
             (numberOfTanks * 512);
         data.experience += dmToMint;
         DarkMatter.mint(msg.sender, dmToMint);
@@ -158,23 +163,23 @@ contract SpaceStation is Ownable, ReentrancyGuard {
         uint256 _vidya,
         uint256 _dm
     ) external nonReentrant {
-        collectionData storage data = astronauts[_tokenID];
+        CollectionData storage data = astronauts[_tokenID];
         require(data.registered, "Space Station: Astronaut not registered.");
-        Item memory stats = Inventory.allItems[_tokenID];
+        IInventory.Item memory stats = Inventory.allItems(_tokenID);
 
         uint256[4] memory xpGained;
         bool adjustNFT;
 
-        if (_vidya > 0 && data.feature[0] < 256) {
+        if (_vidya > 0 && data.features[0] < 256) {
             require(
                 Vidya.balanceOf(msg.sender) >= _vidya,
                 "Space Station: Misscalculation on Vidya amount."
             );
-            Vidya.safeTrasferFrom(msg.sender, vault, _vidya);
+            Vidya.safeTransferFrom(msg.sender, vault, _vidya);
             xpGained[0] = _vidya;
         }
 
-        if (_dm > 0 && data.feature[1] < 256) {
+        if (_dm > 0 && data.features[1] < 256) {
             require(
                 DarkMatter.balanceOf(msg.sender) >= _dm,
                 "Space Station: Misscalculation on Dark Matter token."
@@ -183,110 +188,110 @@ contract SpaceStation is Ownable, ReentrancyGuard {
             xpGained[1] = _dm / 2;
         }
 
-        if (_O2tanks > 0 && data.feature[2] < 256) {
+        if (_O2tanks > 0 && data.features[2] < 256) {
             require(
-                Inventory.balanceOf(msg.sender, O2tanksID) >= _O2tanks,
+                Inventory.balanceOf(msg.sender, O2tanksId) >= _O2tanks,
                 "Space Station: Misscalculation on O2 tanks."
             );
-            Inventory.burn(msg.sender, O2tanksID, _O2tanks);
+            Inventory.burn(msg.sender, O2tanksId, _O2tanks);
             xpGained[2] = (_O2tanks * (10**20));
         }
 
-        if (_numDMNFT > 0 && data.feature[3] < 256) {
+        if (_numDMNFT > 0 && data.features[3] < 256) {
             require(
-                Inventory.balanceOf(msg.sender, DMID) >= _numDMNFT,
+                Inventory.balanceOf(msg.sender, dmId) >= _numDMNFT,
                 "Space Station: Misscalculation on Dark Matter NFTs."
             );
-            Inventory.burn(msg.sender, DMID, _numDMNFT);
+            Inventory.burn(msg.sender, dmId, _numDMNFT);
             adjustNFT = true;
             xpGained[3] = _numDMNFT;
         }
 
-        xpGained[0] += data.experience[0];
-        xpGained[1] += data.experience[1];
-        xpGained[2] += data.experience[2];
+        xpGained[0] += data.experiences[0];
+        xpGained[1] += data.experiences[1];
+        xpGained[2] += data.experiences[2];
 
         for (uint256 index = 0; index < 3; index++) {
             while (
-                xpGained[index] > LVLS[data.levels[index]] &&
+                xpGained[index] > levels[data.levels[index]] &&
                 data.levels[index] < 100
             ) {
-                xpGained[index] = xpGained[index] - LVLS[data.levels[index]];
+                xpGained[index] = xpGained[index] - levels[data.levels[index]];
                 data.levels[index] += 1;
                 adjustNFT = true;
             }
-            data.feature[index] = (data.levels[index] * 255) / 100;
-            data.experience[index] = xpGained[index];
+            data.features[index] = (data.levels[index] * 255) / 100;
+            data.experiences[index] = xpGained[index];
         }
-        data.feature[3] += xpGained[3];
+        data.features[3] += xpGained[3];
         if (adjustNFT) {
             Inventory.changeFeaturesForItem(
                 _tokenID,
-                uint8(data.feature[0]),
-                uint8(data.feature[1]),
-                uint8(data.feature[2]),
-                uint8(data.feature[3]),
+                uint8(data.features[0]),
+                uint8(data.features[1]),
+                uint8(data.features[2]),
+                uint8(data.features[3]),
                 stats.equipmentPosition,
                 msg.sender
             );
             data.rate =
-                (data.feature[0] + data.feature[1] + 1) *
-                (50 + data.feature[3]);
+                (data.features[0] + data.features[1] + 1) *
+                (50 + data.features[3]);
         }
 
-        emit Training(msg.sender, data.feature, _tokenID);
+        emit Training(msg.sender, data.features, _tokenID);
     }
 
     function trainAstronautXP(uint256[3] memory xpGained, uint256 _tokenID)
         external
         nonReentrant
     {
-        collectionData storage data = astronauts[_tokenID];
+        CollectionData storage data = astronauts[_tokenID];
         require(data.registered, "Space Station: Astronaut not registered.");
-        Item memory stats = Inventory.allItems[_tokenID];
+        IInventory.Item memory stats = Inventory.allItems(_tokenID);
 
         bool adjustNFT;
 
-        xpGained[0] += data.experience[0];
-        xpGained[1] += data.experience[1];
-        xpGained[2] += data.experience[2];
+        xpGained[0] += data.experiences[0];
+        xpGained[1] += data.experiences[1];
+        xpGained[2] += data.experiences[2];
 
         for (uint256 index = 0; index < 3; index++) {
             while (
-                xpGained[index] > LVLS[data.levels[index]] &&
+                xpGained[index] > levels[data.levels[index]] &&
                 data.levels[index] < 100
             ) {
-                xpGained[index] = xpGained[index] - LVLS[data.levels[index]];
+                xpGained[index] = xpGained[index] - levels[data.levels[index]];
                 data.levels[index] += 1;
                 adjustNFT = true;
             }
-            data.feature[index] = (data.levels[index] * 255) / 100;
-            data.experience[index] = xpGained[index];
+            data.features[index] = (data.levels[index] * 255) / 100;
+            data.experiences[index] = xpGained[index];
         }
 
         if (adjustNFT) {
             Inventory.changeFeaturesForItem(
                 _tokenID,
-                uint8(data.feature[0]),
-                uint8(data.feature[1]),
-                uint8(data.feature[2]),
-                uint8(data.feature[3]),
+                uint8(data.features[0]),
+                uint8(data.features[1]),
+                uint8(data.features[2]),
+                uint8(data.features[3]),
                 stats.equipmentPosition,
                 msg.sender
             );
             data.rate =
-                (data.feature[0] + data.feature[1] + 1) *
-                (50 + data.feature[3]);
+                (data.features[0] + data.features[1] + 1) *
+                (50 + data.features[3]);
         }
 
-        emit Training(msg.sender, data.feature, _tokenID);
+        emit Training(msg.sender, data.features, _tokenID);
     }
 
     function resupply(uint256 _tokenID, uint256 _O2tanks)
         external
         nonReentrant
     {
-        collectionData storage data = astronauts[_tokenID];
+        CollectionData storage data = astronauts[_tokenID];
 
         require(data.registered, "Space Station: Astronaut not registered");
         require(
@@ -294,36 +299,36 @@ contract SpaceStation is Ownable, ReentrancyGuard {
             "Space Station: Astronaut in Orbit"
         );
         require(
-            Inventory.balanceOf(msg.sender, O2tanksID) >= _O2tanks,
+            Inventory.balanceOf(msg.sender, O2tanksId) >= _O2tanks,
             "Space Station: Misscalculation on supplies"
         );
 
         uint256 timeDifference = (data.collectionStart - block.timestamp) /
             (2 * _O2tanks);
-        Inventory.burn(msg.sender, O2tanksID, _O2tanks);
+        Inventory.burn(msg.sender, O2tanksId, _O2tanks);
         data.collectionStart = timeDifference + block.timestamp;
 
         //       emit Resupply(msg.sender, _tokenID, data.collectionStart);
     }
 
     //Needs to pass in the actual value like 10**18 for 1.
-    function changeBaseRate(uint256 _DMperBlock) external onlyOwner {
-        dmPerBlockish = _DMperBlock;
-        adjustedDMRate = dmPerBlockish / numberAstorsnauts;
+    function changeBaseRate(uint256 _dmperBlock) external onlyOwner {
+        dmPerBlock = _dmperBlock;
+        adjustedDmRate = dmPerBlock / numberAstorsnauts;
 
-        emit RateChange(adjustedDMRate);
+        emit RateChange(adjustedDmRate);
     }
 
     function createLevels() external onlyOwner {
-        require(LVLS[99] == 0, "Space Station: Levels already Calculated.");
+        require(levels[99] == 0, "Space Station: Levels already Calculated.");
 
         uint256 index = 4;
         uint256 deci = 10**20;
-        while (LVLS[99] == 0) {
-            LVLS[index] = LVLS[index - 1] + ((index + 1) * deci);
+        while (levels[99] == 0) {
+            levels[index] = levels[index - 1] + ((index + 1) * deci);
             index += 1;
         }
 
-        emit LevelsCreated(LVLS);
+        emit LevelsCreated(levels);
     }
 }
--- contracts/interfaces/IInventory.sol
+++ contracts/interfaces/IInventory.sol
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.6;
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 /**
  * @title Inventory Interface
  */
-interface IInventory {
+interface IInventory is IERC1155 {
+    struct Item {
+        uint256 templateId; // id of Template in the itemTemplates array
+        uint8 feature1;
+        uint8 feature2;
+        uint8 feature3;
+        uint8 feature4;
+        uint8 equipmentPosition;
+        bool burned;
+    }
+
     /**
      * @dev External function to create item from templates. This function can be called by approved games only.
      * @param _templateId Id of template
@@ -50,5 +61,31 @@ interface IInventory {
         address _user,
         uint256 _tokenId,
         uint256 _amount
+    ) external;
+
+    /**
+     * @dev External function to get the item by token Id.
+     * @param _tokenId Token id
+     */
+    function allItems(uint256 _tokenId) external returns (Item memory);
+
+    /**
+     * @dev External function to change features for item. This function can be called by approved games only.
+     * @param _tokenId Id of Token
+     * @param _feature1 Feature 1
+     * @param _feature2 Feature 2
+     * @param _feature3 Feature 3
+     * @param _feature4 Feature 4
+     * @param _equipmentPosition Equipment position
+     * @param _player Address of player
+     */
+    function changeFeaturesForItem(
+        uint256 _tokenId,
+        uint8 _feature1,
+        uint8 _feature2,
+        uint8 _feature3,
+        uint8 _feature4,
+        uint8 _equipmentPosition,
+        address _player
     ) external;
 }
